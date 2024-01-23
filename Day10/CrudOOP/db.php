3c4e2a038e88316548a1dec2847016a3d40f3f90
hiten <chotaliyahiten672@gmail.com>
2024-01-23 12:41:10 +0530

    Pagination with OOP

--- Day10/CrudOOP/db.php
+++ Day10/CrudOOP/db.php
@@ -1,5 +1,9 @@
 <?php
-class Database{
+
+use function PHPSTORM_META\type;
+
+class Database
+{
 
     private $db_host = "localhost";
     private $db_user = "root";
@@ -9,6 +13,7 @@ class Database{
     private $mysqli = "";
     private $result = array();
     private $conn = false;
+
 
     public function __construct()
     {
@@ -24,17 +29,17 @@ class Database{
             return true;
         }
     }
-    
+
 
     //Close connection
     public function  __destruct()
     {
-        if($this->conn){
-            if($this->mysqli->close()){
+        if ($this->conn) {
+            if ($this->mysqli->close()) {
                 $this->conn = false;
                 return true;
             }
-        }else{
+        } else {
             return false;
         }
     }
@@ -43,7 +48,7 @@ class Database{
     {
         $sql = "SHOW TABLES FROM $this->db_name LIKE '$TableName'";
         $TableInDB = $this->mysqli->query($sql);
-    
+
         if ($TableInDB !== false) {
             if ($TableInDB->num_rows == 1) {
                 return true;
@@ -57,39 +62,192 @@ class Database{
             return false;
         }
     }
-    
-    public function Show() {
-        
+    public function GetResult()
+    {
+        $StoreData = $this->result;
+        $this->result = array();
+        return $StoreData;
     }
 
-    public function Insert($TableName, $Values=array()) {
-        if($this->TableExist($TableName)){
-            print_r($Values);
-
-            $table_columns = implode(',',array_keys($Values));
-            $table_value = implode("','",$Values);
-            $sql="INSERT into $TableName($table_columns) Values ('$table_value')";
-            // echo $sql;
-            if($this->mysqli->query($sql)){
-                array_push($this->result,$this->mysqli->insert_id);
-                return true;
-            }else{
-                array_push($this->result,$this->mysqli->error);
-                return false;
+    public function Select($table, $rows = "*", $join = null, $where = null, $orderby = null, $limit = null)
+    {
+        if ($this->TableExist($table)) {
+            $sql = "SELECT $rows FROM $table";
+            if ($join != null) {
+                $sql .= " JOIN $join";
+            }
+            if ($where != null) {
+                $sql .= " WHERE $where";
+            }
+            if ($orderby != null) {
+                $sql .= " ORDER BY $orderby";
+            }
+            if ($limit != null) {
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                } else {
+                    $page = 1;
+                }
+                $start = ($page - 1) * $limit;
+                $sql .= " LIMIT $start,$limit";
             }
 
-        }else{
+            echo $sql;
+            $query = $this->mysqli->query($sql);
+            if ($query) {
+                $this->result = $query->fetch_all(MYSQLI_ASSOC);
+                return true;
+            } else {
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+        } else {
+            array_push($this->result, $this->mysqli->error);
+        }
+    }
+    public function sql($sql)
+    {
+        $query = $this->mysqli->query($sql);
+
+        if ($query) {
+            $this->result = $query->fetch_all(MYSQLI_ASSOC);
+            return true;
+        } else {
+            array_push($this->result, $this->mysqli->error);
             return false;
         }
     }
-    
-    public function Update() {
-        
-    }
-    
-    public function Delete() {
-        
+    public function Insert($TableName, $Values = array())
+    {
+        if ($this->TableExist($TableName)) {
+            print_r($Values);
+
+            $table_columns = implode(',', array_keys($Values));
+            $table_value = implode("','", $Values);
+            $sql = "INSERT into $TableName($table_columns) Values ('$table_value')";
+            // echo $sql;
+            if ($this->mysqli->query($sql)) {
+                array_push($this->result, $this->mysqli->insert_id);
+                return true;
+            } else {
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
+    public function Update($TableName, $Values = array(), $Where = "")
+    {
+        if ($this->TableExist($TableName)) {
+
+            // print_r($Values);
+            $args = array();
+            foreach ($Values as $key => $value) {
+                $args[] = "$key= '$value'";
+            }
+            echo "<pre>";
+            print_r($args);
+            $sql = "UPDATE $TableName SET " . implode(',', $args);
+            if ($Where != null) {
+                $sql .= "WHERE $Where";
+            }
+            if ($this->mysqli->query($sql)) {
+                array_push($this->result, $this->mysqli->affected_rows);
+                return true;
+            } else {
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+            // echo $sql;
+        } else {
+            return false;
+        }
+    }
+
+    public function Delete($TableName, $Where = null)
+    {
+        if ($this->TableExist($TableName)) {
+            $sql = "DELETE FROM $TableName ";
+            if ($Where != null) {
+                $sql .= "WHERE $Where";
+            }
+            if ($this->mysqli->query($sql)) {
+                array_push($this->result, $this->mysqli->affected_rows);
+                return true;
+            } else {
+                array_push($this->result, $this->mysqli->error);
+                return false;
+            }
+            echo $sql;
+        } else {
+            return false;
+        }
+    }
+
+    public function Pagination($table, $join = null, $where = null, $limit = null)
+    {
+        if ($this->TableExist($table)) {
+            if ($limit != null) {
+                // Count total records
+                $sql = "SELECT COUNT(*) FROM $table";
+
+                if ($join != null) {
+                    $sql .= " JOIN $join";
+                }
+                if ($where != null) {
+                    $sql .= " WHERE $where";
+                }
+                $query = $this->mysqli->query($sql);
+
+                // Get total records count
+                $TotalRecords = $query->fetch_array();
+                $TotalRecords = $TotalRecords[0];
+
+                // Calculate total pages based on the limit
+                $TotalPage = ceil($TotalRecords / $limit);
+
+                // Get the current page from the URL
+                $url = basename($_SERVER['PHP_SELF']);
+
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                } else {
+                    $page = 1;
+                }
+
+
+                $TResult = "<ul class='pagination'>";
+
+                // Display Previous button if not on the first page
+                if ($page > 1) {
+                    $TResult .= "<li><a href='$url?page=" . ($page - 1) . "'>Prev</a></li>";
+                }
+
+                // Loop for displaying page numbers
+                for ($i = 1; $i <= $TotalPage; $i++) {
+                    if ($i == $page) {
+                        $cls = "class='active' style='color:red'";
+                    } else {
+                        $cls = "";
+                    }
+                    // Page number links
+                    $TResult .= "<li><a $cls href='$url?page=$i'>$i</a></li>";
+                }
+                // Display Next button if not on the last page
+                if ($page < $TotalPage) {
+                    $TResult .= "<li><a href='$url?page=" . ($page + 1) . "'>Next</a></li>";
+                }
+                $TResult .= "</ul'>";
+
+
+                return $TResult;
+            } else {
+                return false; // Pagination is not applicable without a limit
+            }
+        } else {
+            array_push($this->result, $this->mysqli->error);
+        }
+    }
 }
-
--- Day10/CrudOOP/index.php
+++ Day10/CrudOOP/index.php
@@ -4,4 +4,39 @@ include './db.php';
 
 $ConnObj = new Database();
 
-$ConnObj->Insert('demos',["fname"=>"Rakesh","lname"=>"Kir"]);
+$join = "city ON demo.cid = city.cid";
+$ColumToShow = "demo.id,demo.fname,demo.lname,city.name";
+$limit='3';
+
+//INSERT
+// $ConnObj->Insert('demo',["fname"=>"Rakesh","lname"=>"Kir"]);
+// echo "Insert result is: ";
+// print_r($ConnObj->GetResult());
+
+//UPDATE
+// $ConnObj->Update('demo',["fname"=>"Milan","lname"=>"Patel"],'id="19"');
+// echo "Update result is: ";
+// print_r($ConnObj->GetResult());
+
+//Delete
+// $ConnObj->Delete('demo','id="20"');
+// echo "Update result is: ";
+// print_r($ConnObj->GetResult()); 
+
+//SQL Select
+// $ConnObj->sql('SELECT * FROM demo');
+// // $ConnObj->sql('SELECT * FROM demo WHERE fname = "Mitesh"');
+// echo "SQL result is: ";
+// echo "<pre>";
+// print_r($ConnObj->GetResult()); 
+
+//Select dynamic
+// $ConnObj->Select('demo','fname',null,'fname LIKE "%a%"','fname','5');
+// $ConnObj->Select('demo','*',$join,null,null,'2');
+$ConnObj->Select('demo', $ColumToShow, $join, null, null, $limit);
+echo "<br>Select result is: ";
+echo "<pre>";
+print_r($ConnObj->GetResult());
+
+//Pagination
+echo $ConnObj->Pagination('demo', $join, null, $limit);
